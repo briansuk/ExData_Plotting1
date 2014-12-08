0d6517434ae07721180ce600753232b4ff2663d3
briansuk <briansuk@gmail.com>
2014-12-07 21:53:18 -0500

    plot 2

---
+++ plot3.R
@@ -0,0 +1,6 @@
+subDF <- household_power_consumption[as.Date(household_power_consumption$Date, "%d/%m/%Y") >= as.Date("2007-02-01", "%Y-%m-%d") & as.Date(household_power_consumption$Date, "%d/%m/%Y") <= as.Date("2007-02-02", "%Y-%m-%d"),c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3", "Date", "Time")]
+gapDF$Date <- strptime(paste(as.character(gapDF$Date), as.character(gapDF$Time), sep = " "), "%d/%m/%Y %H:%M:%S")
+gapDF$Global_active_power <- as.numeric(gapDF$Global_active_power)
+plot(gapDF$Date, gapDF$Global_active_power/1000, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.copy(png, 'plot2.png')
+dev.off()
